9379a535be54ec451b69de0c87cabd6ac96a50bf
Tiago Gomes <tiagogomes187@gmail.com>
2022-05-09 22:24:02 -0300

    MovieDetails responsive

--- frontweb/src/pages/MovieDetails/index.tsx
+++ frontweb/src/pages/MovieDetails/index.tsx
@@ -10,15 +10,15 @@ const MovieDetails = () => {
                 <div className="img-container">
                   <img
                     src="https://image.tmdb.org/t/p/w533_and_h300_bestv2/wu1uilmhM4TdluKi2ytfz8gidHf.jpg"
-                    alt="Nome do filme"
+                    alt="O Retorno do Rei"
                   />
                 </div>
               </div>
               <div className="col-xl-6">
                 <div className="title-subtitle-container">
-                  <h1>Nome do filme</h1>
+                  <h1>O Retorno do Rei</h1>
                   <h6>2013</h6>
-                  <p>Subtitulo do filme</p>
+                  <p>O olho do inimigo está se movendo.</p>
                 </div>
                 <div className="sinopse-container">
                   <p>O confronto final entre as forças do bem e do mal que lutam pelo controle do futuro da Terra Média se aproxima. Sauron planeja um grande ataque a Minas Tirith, capital de Gondor, o que faz com que Gandalf e Pippin partam para o local na intenção de ajudar a resistência. Um exército é reunido por Theoden em Rohan, em mais uma tentativa de deter as forças de Sauron. Enquanto isso, Frodo, Sam e Gollum seguem sua viagem rumo à Montanha da Perdição para destruir o anel.</p>
