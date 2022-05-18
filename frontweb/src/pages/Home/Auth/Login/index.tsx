681a845f053a2cd96463d238420fbd5b16cb3fc5
Tiago Gomes <tiagogomes187@gmail.com>
2022-05-17 21:49:41 -0300

    Manual error handlig

--- frontweb/src/pages/Home/Auth/Login/index.tsx
+++ frontweb/src/pages/Home/Auth/Login/index.tsx
@@ -1,4 +1,5 @@
 import ButtonIcon from 'components/ButtonIcon';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { requestBackendLogin } from 'util/requests';
 
@@ -10,20 +11,28 @@ type FormData = {
 };
 
 const Login = () => {
+  const [hasError, setHasError] = useState(false);
   const { register, handleSubmit } = useForm<FormData>();
 
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
       .then((response) => {
+        setHasError(false);
         console.log('SUCESSO!', response);
       })
       .catch((error) => {
+        setHasError(true);
         console.log('ERRO', error);
       });
   };
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
+      {hasError && (
+        <div className="alert alert-danger">
+          Erro ao tentar efetuar o login
+        </div>
+      )}
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
