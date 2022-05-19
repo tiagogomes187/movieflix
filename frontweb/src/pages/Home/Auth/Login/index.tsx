bb76e61754cb679ceeaca57fded465d9bcba161b
Tiago Gomes <tiagogomes187@gmail.com>
2022-05-18 21:39:24 -0300

    Conditional styling

--- frontweb/src/pages/Home/Auth/Login/index.tsx
+++ frontweb/src/pages/Home/Auth/Login/index.tsx
@@ -12,7 +12,11 @@ type FormData = {
 
 const Login = () => {
   const [hasError, setHasError] = useState(false);
-  const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
@@ -38,28 +42,32 @@ const Login = () => {
               required: 'Campo obrigatório',
               pattern: {
                 value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: 'Email inválido'
-              }
+                message: 'Email inválido',
+              },
             })}
             type="text"
-            className="form-control base-input"
+            className={`form-control base-input ${errors.username ? 'is-invalid' : ''}`}
             placeholder="Email"
             name="username"
           />
-          <div className="invalid-feedback d-block">{errors.username?.message}</div>
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
         </div>
 
         <div className="mb-2">
           <input
             {...register('password', {
-              required: 'Campo obrigatório'
+              required: 'Campo obrigatório',
             })}
             type="password"
-            className="form-control base-input"
+            className={`form-control base-input ${errors.password ? 'is-invalid' : ''}`}
             placeholder="Password"
             name="password"
           />
-          <div className="invalid-feedback d-block">{errors.password?.message}</div>
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
+          </div>
         </div>
 
         <div className="login-submit">
