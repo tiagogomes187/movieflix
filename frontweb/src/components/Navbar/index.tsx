919c3cad4b45e33686edda706644b6a1d862cd57
Tiago Gomes <tiagogomes187@gmail.com>
2022-06-01 22:04:11 -0300

    Login/logout styling

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -52,10 +52,10 @@ const Navbar = () => {
             <h4>MovieFlix</h4>
           </NavLink>
 
-          <div>
+          <div className="nav-login-logout">
             {authData.authenticated ? (
               <>
-                <span>{authData.tokenData?.user_name}</span>
+                <span className="nav-username">{authData.tokenData?.user_name}</span>
                 <a href="#logout" onClick={handleLogoutClick}>
                   SAIR
                 </a>
