f2d423caf477688fe58f266da07ad1cb6892a0fb
Tiago Gomes <tiagogomes187@gmail.com>
2022-06-01 23:37:09 -0300

    Redirecting to intended route after login

--- frontweb/src/components/PrivateRoute/index.tsx
+++ frontweb/src/components/PrivateRoute/index.tsx
@@ -1,21 +1,29 @@
 import { Redirect, Route } from 'react-router-dom';
 import { isAuthenticated } from 'util/requests';
- 
+
 type Props = {
   children: React.ReactNode;
   path: string;
 };
- 
+
 const PrivateRoute = ({ children, path }: Props) => {
- 
   return (
     <Route
       path={path}
-      render={() =>
-        !isAuthenticated() ? (<Redirect to="/auth/login" />) : (<>{children}</>)
+      render={({ location }) =>
+        !isAuthenticated() ? (
+          <Redirect
+            to={{
+              pathname: '/auth/login',
+              state: { from: location },
+            }}
+          />
+        ) : (
+          <>{children}</>
+        )
       }
     />
   );
 };
- 
+
 export default PrivateRoute;
--- frontweb/src/pages/Home/Auth/Login/index.tsx
+++ frontweb/src/pages/Home/Auth/Login/index.tsx
@@ -2,7 +2,7 @@ import { AuthContext } from 'AuthContext';
 import ButtonIcon from 'components/ButtonIcon';
 import { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { getTokenData, requestBackendLogin, saveAuthData } from 'util/requests';
 
 import './styles.css';
@@ -12,7 +12,14 @@ type FormData = {
   password: string;
 };
 
+type LocationState = {
+  from: string;
+};
+
 const Login = () => {
+  const location = useLocation<LocationState>();
+
+  const { from } = location.state || { from: { pathname: '/movies' } };
 
   const { setAuthContextData } = useContext(AuthContext);
 
@@ -33,8 +40,8 @@ const Login = () => {
         setAuthContextData({
           authenticated: true,
           tokenData: getTokenData(),
-        })
-        history.push('/movies')
+        });
+        history.replace(from);
       })
       .catch((error) => {
         setHasError(true);
